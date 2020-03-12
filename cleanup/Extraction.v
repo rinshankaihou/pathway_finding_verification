4848c78cc9969cd709339efc7739c242c2f06e0e
dk <madoka@umich.edu>
2020-03-12 14:43:13 -0400

    change folder name to src

--- cleanup/Extraction.v
+++
@@ -1,9 +0,0 @@
-(* 
-    run this file in order to extract to ocaml
-    In default, this file won't be compiled use "make"
-*)
-
-From Taxiway Require Export Implementation.
-
-Require Coq.extraction.Extraction.
-Extraction "find_path.ml" find_path.
