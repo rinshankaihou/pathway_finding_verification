5a7969d5b6a23ded3a198f6b0d880825cc6ca51f
Siyuan He <hesy@umich.edu>
2020-04-30 23:48:56 +0800

    5 downward theorems

--- cleanup/Downward.v
+++ cleanup/Downward.v
@@ -1,3 +1,4 @@
+From mathcomp Require Import all_ssreflect.
 Require Import Coq.Strings.String Coq.Bool.Bool Coq.Lists.List.
 Import ListNotations.
 From Coq.Arith Require Import Arith EqNat.
@@ -14,6 +15,8 @@ From Hammer Require Import Hammer.
 
 (* This file have five theorems indicating the theorem of complete graph preserve downward*)
 
+(* start correct *)
+
 Definition naive_path_starts_with_vertex (path : list Edge_type) (start_v : Vertex) : Prop := 
     exists taxiway_name, exists l, path = ((start_v, input), taxiway_name) :: l.
 
@@ -21,5 +24,86 @@ Definition naive_path_starts_with_vertex (path : list Edge_type) (start_v : Vert
 Theorem naive_start_correct:
     forall start_v end_v ATC D (path : list Arc_type) (paths : list (list Arc_type)),
     Some paths = (find_path (start_v : Vertex) (end_v : Vertex) (ATC : list string) (D : C_Graph_type)) ->
-    In path (to_N paths) ->
-    naive_path_starts_with_vertex path start_v.
+    In path paths ->
+    naive_path_starts_with_vertex (to_N_path path) start_v.
+Proof. Admitted.
+
+
+
+(* end correct*)
+
+Definition naive_ends_with_vertex (path : list Edge_type) (end_v : Vertex) : Prop :=
+    exists end_Edge,
+    ((hd_error (rev path)) = Some end_Edge) /\ end_Edge.1.1 = end_v.
+
+
+Theorem output_path_end_correct:
+    forall start_v end_v ATC D (path : list Arc_type) (paths : list (list Arc_type)),
+    Some paths = (find_path (start_v : Vertex) (end_v : Vertex) (ATC : list string) (D : C_Graph_type)) ->
+    In path paths ->
+    naive_ends_with_vertex (to_N_path path) end_v.
+Proof. Admitted.
+
+
+(* in graph *)
+
+
+
+Definition naive_path_in_graph (path : list Edge_type) (G : list Edge_type) : Prop :=
+    forall a, In a (tl (path)) -> In a G.
+
+
+Theorem naive_in_graph : 
+    forall start_v end_v ATC D (path : list Arc_type) (paths : list (list Arc_type)),
+    Some paths = (find_path (start_v : Vertex) (end_v : Vertex) (ATC : list string) (D : C_Graph_type)) ->
+    In path paths ->
+    naive_path_in_graph (to_N_path path) (to_N D).
+Proof. Admitted.
+
+
+(* connected *)
+
+Definition Edge_conn (e1 : Edge_type) (e2 : Edge_type) : Prop :=
+    e1.1.1 = e2.1.2.
+
+Fixpoint naive_path_conn (path : list Edge_type): Prop :=
+    match path with
+    | path_f::path_r => match path_r with
+        | path_s::path_r_r => (Edge_conn path_f path_s) /\ (naive_path_conn path_r)
+        | [] => True
+        end
+    | [] => True
+    end.
+
+Theorem naive_conn:
+    forall start_v end_v ATC D (path : list Arc_type) (paths : list (list Arc_type)),
+    Some paths = (find_path (start_v : Vertex) (end_v : Vertex) (ATC : list string) (D : C_Graph_type)) ->
+    In path paths ->
+    naive_path_conn (rev (to_N_path path)).
+Proof. Admitted.
+
+
+(* follow ATC *)
+
+Fixpoint naive_path_coresp_atc (path : list Edge_type) : list string :=
+    match path with
+    | [] => []
+    | a::b => match b with
+        | []   => [a.2]
+        | c::l => if (a.2 =? c.2) 
+        then (naive_path_coresp_atc b)
+        else a.2::(naive_path_coresp_atc b)
+        end 
+    end.
+
+
+Definition naive_path_follow_atc (path : list Edge_type) (atc : list string) : Prop :=
+    atc = naive_path_coresp_atc path.
+
+
+Theorem naive_follow_atc:
+    forall start_v end_v ATC D (path : list Arc_type) (paths : list (list Arc_type)),
+    Some paths = (find_path (start_v : Vertex) (end_v : Vertex) (ATC : list string) (D : C_Graph_type)) ->
+    In path paths ->
+    naive_path_follow_atc (to_N_path path) ATC.
+Proof. Admitted.
